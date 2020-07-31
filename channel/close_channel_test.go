edfd5be4f0ad7b47ff2405adc2570f8fe4a42ef4
link-yundi <link.yundi@foxmail.com>
2020-07-31 16:44:42 +0800

    add comment about close(channel)

--- channel/close_channel_test.go
+++ channel/close_channel_test.go
@@ -7,7 +7,7 @@ import "testing"
 func TestCloseChannel(t *testing.T) {
 	// 关闭语法：close(channel)
 	// 数据没有读取完，不应该关闭channel
-	// 关闭的channel 写入数据会引发错误，但是可以读取数据，读取的零值（默认值）
+	// 关闭的channel 写入数据会引发错误，但是可以读取数据，读取的零值（默认值），也就是说，close(channel)关闭的是读端
 	// value, ok := <- channel
 	// ok 为false channel已经关闭，true 则channel没有关闭
 	ch := make(chan int)
